d9d33f02c860d0a2fae83051623279ec315b502a
DrinCSO <dkryeziu464@gmail.com>
2024-11-07 19:19:26 +0100

    Create index.php

---
+++ Module_7/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//Setting MySQL database parameters
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+
+//Connection in datatbase using PDO
+//Beinning of a try block
+try{
+    //Attemt to create a neq PDO object and connecting to a MySQL database
+    //The connection string is contructed using the variables $host, $user and $pass
+
+    $conn = new PDO("mysql:host=$host, $user, $pass");
+
+    //If the connection is succesful
+    echo "Connected";
+} catch (Exception $e){
+    echo "Not Connected";
+}
+?> 

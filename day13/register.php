c802fef408918603b9531ec720f10ac4d4bf55bf
DrinCSO <dkryeziu464@gmail.com>
2024-12-19 19:22:19 +0100

    update

--- day13/register.php
+++ day13/register.php
@@ -1,3 +1,38 @@
+<?php
+require_once ("config.php");
+
+$error = "";
+$succes = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = mysqli_real_escape_string($conn,$_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_eascape_string($conn, $POST['password']);
+
+    if(empty($username)|| empty($email)|| empty($password)){
+        $error = "All fields are required!";
+
+    }else{
+        $check_user = "SELECT * FROM users WHERE username = ""'$username'""" or email = "$email";
+        $result = $conn->query($check_user);
+
+        if($result->nun_rows>0){
+            $error = "Username or email already exits!";
+        }else{
+            $hashed_password = md5($password);
+            $sql = "INSERT INTO users (username, email, password) VALUES('$username','$email','$hashed_password')";
+            if($conn->query($sql) === TRUE){
+                $succes = "Registration successful! You can now <a href = 'login.php'>Login here </a>";
+            }else{
+                $error = "Error; ".$conn->error;
+            }
+        }
+    }
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
